cacfe332ec95a52481af6ff67e3d9f600805248f
srikanth <srikanthsheru730@gmail.com>
2024-07-27 15:20:12 +0530

    First commit

---
+++ src/StudentDatabaseApp/Main.java
@@ -0,0 +1,13 @@
+package StudentDatabaseApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student stu1 = new Student();
+		//ask how many users we want to add
+
+		//create a number of new students
+
+	}
+
+}
